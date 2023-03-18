1a9e97cfe82c7ef50d80c2155d9fd8240b8f83ed
aramamba <aramamba@PBIO-2827.bbdo.org>
2023-03-18 20:10:00 +0400

    Etat initial

---
+++ src/main/java/com/mbelo/tpcustomer/resources/JakartaEE10Resource.java
@@ -0,0 +1,20 @@
+package com.mbelo.tpcustomer.resources;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.core.Response;
+
+/**
+ *
+ * @author 
+ */
+@Path("jakartaee10")
+public class JakartaEE10Resource {
+    
+    @GET
+    public Response ping(){
+        return Response
+                .ok("ping Jakarta EE")
+                .build();
+    }
+}
